3f6cd3dc20ff6f3f6e80ac3627bf3d1b08f2e434
rlk <rlk>
2003-09-21 02:37:02 +0000

    Preliminary support for Epson Stylus C63/C64 and PX-V500

--- src/main/print-escp2.h
+++ src/main/print-escp2.h
@@ -1,5 +1,5 @@
 /*
- * "$Id: print-escp2.h,v 1.66 2003/09/20 23:44:32 rlk Exp $"
+ * "$Id: print-escp2.h,v 1.67 2003/09/21 02:37:02 rlk Exp $"
  *
  *   Print plug-in EPSON ESC/P2 driver for the GIMP.
  *
@@ -481,11 +481,11 @@ extern const escp2_drop_list_t stpi_escp2_variable_2000p_drops;
 extern const escp2_drop_list_t stpi_escp2_variable_x80_6pl_drops;
 
 extern const paperlist_t stpi_escp2_standard_paper_list;
-extern const paperlist_t stpi_escp2_c80_paper_list;
+extern const paperlist_t stpi_escp2_durabrite_paper_list;
 extern const paperlist_t stpi_escp2_ultrachrome_paper_list;
 
 extern const paper_adjustment_list_t stpi_escp2_standard_paper_adjustment_list;
-extern const paper_adjustment_list_t stpi_escp2_c80_paper_adjustment_list;
+extern const paper_adjustment_list_t stpi_escp2_durabrite_paper_adjustment_list;
 extern const paper_adjustment_list_t stpi_escp2_photo_paper_adjustment_list;
 extern const paper_adjustment_list_t stpi_escp2_photo2_paper_adjustment_list;
 extern const paper_adjustment_list_t stpi_escp2_sp960_paper_adjustment_list;
@@ -508,6 +508,7 @@ extern const res_t *const stpi_escp2_sc640_reslist[];
 extern const inkgroup_t stpi_escp2_cmy_inkgroup;
 extern const inkgroup_t stpi_escp2_standard_inkgroup;
 extern const inkgroup_t stpi_escp2_c80_inkgroup;
+extern const inkgroup_t stpi_escp2_c64_inkgroup;
 extern const inkgroup_t stpi_escp2_x80_inkgroup;
 extern const inkgroup_t stpi_escp2_photo_gen1_inkgroup;
 extern const inkgroup_t stpi_escp2_photo_gen2_inkgroup;
@@ -627,5 +628,5 @@ extern void stpi_escp2_terminate_page(stp_vars_t v);
 
 #endif /* GIMP_PRINT_INTERNAL_ESCP2_H */
 /*
- * End of "$Id: print-escp2.h,v 1.66 2003/09/20 23:44:32 rlk Exp $".
+ * End of "$Id: print-escp2.h,v 1.67 2003/09/21 02:37:02 rlk Exp $".
  */
