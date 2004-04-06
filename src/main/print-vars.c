717830b93fc3180532ebbd0ec8533cafe2a0f95a
rlk <rlk>
2004-04-06 23:41:18 +0000

    Color conversion name should *not* be translated.

--- src/main/print-vars.c
+++ src/main/print-vars.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: print-vars.c,v 1.64 2004/03/28 21:17:38 rlk Exp $"
+ * "$Id: print-vars.c,v 1.65 2004/04/06 23:41:18 rlk Exp $"
  *
  *   Print plug-in driver utility functions for the GIMP.
  *
@@ -330,7 +330,7 @@ initialize_standard_vars(void)
       for (i = 0; i < STP_PARAMETER_TYPE_INVALID; i++)
 	default_vars.params[i] = create_vars_list();
       default_vars.driver = stpi_strdup("ps2");
-      default_vars.color_conversion = stpi_strdup(_("traditional"));
+      default_vars.color_conversion = stpi_strdup("traditional");
       default_vars.internal_data = create_compdata_list();
       standard_vars_initialized = 1;
     }
