af3045dead92ab835be549a9767c431f00ac42e1
rlk <rlk>
2004-07-31 17:48:05 +0000

    1000661	GIMP plugin does not refresh correctly in some cases

--- src/libgimpprintui/panel.c
+++ src/libgimpprintui/panel.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: panel.c,v 1.69 2004/07/31 17:19:25 rlk Exp $"
+ * "$Id: panel.c,v 1.70 2004/07/31 17:48:05 rlk Exp $"
  *
  *   Main window code for Print plug-in for the GIMP.
  *
@@ -4154,6 +4154,11 @@ do_preview_thumbnail (void)
       paper_display_height - (paper_display_top - printable_display_top);
   if (need_exposure)
     {
+      if (!frame_valid)
+	{
+	  gdk_window_clear (preview->widget.window);
+	  frame_valid = TRUE;
+	}
       /* draw paper frame */
       gdk_draw_rectangle (preview->widget.window, gc, 0,
 			  paper_display_left, paper_display_top,
