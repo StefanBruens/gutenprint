ac327b920e5070662f93bcd4fa8975b32a8bf11f
rlk <rlk>
2005-03-27 04:18:43 +0000

    Add other supported Ghostscript versions.

--- src/cups/genppd.c
+++ src/cups/genppd.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: genppd.c,v 1.103 2004/11/22 13:31:39 rlk Exp $"
+ * "$Id: genppd.c,v 1.104 2005/03/27 04:18:43 rlk Exp $"
  *
  *   PPD file generation program for the CUPS drivers.
  *
@@ -915,7 +915,15 @@ write_ppd(const stp_printer_t *p,	/* I - Printer driver */
   if (cups_ppd_ps_level == 2)
     gzputs(fp, "*PSVersion:	\"(2017.000) 550\"\n");
   else
-    gzputs(fp, "*PSVersion:	\"(3010.000) 705\"\n");
+    {
+      gzputs(fp, "*PSVersion:	\"(3010.000) 550\"\n");
+      gzputs(fp, "*PSVersion:	\"(3010.000) 651\"\n");
+      gzputs(fp, "*PSVersion:	\"(3010.000) 652\"\n");
+      gzputs(fp, "*PSVersion:	\"(3010.000) 653\"\n");
+      gzputs(fp, "*PSVersion:	\"(3010.000) 704\"\n");
+      gzputs(fp, "*PSVersion:	\"(3010.000) 705\"\n");
+      gzputs(fp, "*PSVersion:	\"(3010.000) 800\"\n");
+    }
   gzprintf(fp, "*LanguageLevel:	\"%d\"\n", cups_ppd_ps_level);
 
   /* Set Job Mode to "Job" as this enables the Duplex option */
@@ -1683,5 +1691,5 @@ write_ppd(const stp_printer_t *p,	/* I - Printer driver */
 
 
 /*
- * End of "$Id: genppd.c,v 1.103 2004/11/22 13:31:39 rlk Exp $".
+ * End of "$Id: genppd.c,v 1.104 2005/03/27 04:18:43 rlk Exp $".
  */
