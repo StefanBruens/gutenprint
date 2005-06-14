6cec0613c7ba24213ca2fe593c60c0ad69dbface
rlk <rlk>
2005-06-14 02:49:09 +0000

    Generate auxiliary channels with curves rather than hue angles.  This yields better results on the R800, since using hue angles means that at certain points only one ink can be used.  The magenta ink on the R800 is very weak, and does not generate a satisfactory magenta. This probably still needs more tuning, but it should yield better results and is easier to generalize.
    Improve tunings of small drop sizes on R800.

--- src/main/print-escp2.h
+++ src/main/print-escp2.h
@@ -1,5 +1,5 @@
 /*
- * "$Id: print-escp2.h,v 1.93 2005/06/07 00:22:23 rlk Exp $"
+ * "$Id: print-escp2.h,v 1.94 2005/06/14 02:49:10 rlk Exp $"
  *
  *   Print plug-in EPSON ESC/P2 driver for the GIMP.
  *
@@ -178,10 +178,17 @@ typedef struct
 
 typedef struct
 {
+  const char *curve_name;
+  const char *curve;
+  stp_curve_t *curve_impl;
+} hue_curve_t;
+
+typedef struct
+{
   const char *listname;
   const physical_subchannel_t *subchannels;
   short n_subchannels;
-  double hue;
+  hue_curve_t *hue_curve;
 } ink_channel_t;
 
 typedef enum
@@ -697,5 +704,5 @@ extern void stpi_escp2_terminate_page(stp_vars_t *v);
 
 #endif /* GUTENPRINT_INTERNAL_ESCP2_H */
 /*
- * End of "$Id: print-escp2.h,v 1.93 2005/06/07 00:22:23 rlk Exp $".
+ * End of "$Id: print-escp2.h,v 1.94 2005/06/14 02:49:10 rlk Exp $".
  */
