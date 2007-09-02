616d52e7b870fcfa9685abdc778c2f1061acc674
rlk <rlk>
2007-09-02 18:14:00 +0000

    Do not offer resolutions above 5760x1440 for quality settings on Epson printers.  Experiments have demonstrated that the bottom of the page prints poorly at 2880x2880 and 5760x2880 on many printers, and Epson does not currently offer resolutions above 5760x1440 on any printers. See bug 1700313.

--- src/main/escp2-resolutions.c
+++ src/main/escp2-resolutions.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: escp2-resolutions.c,v 1.42 2007/05/27 17:59:50 rlk Exp $"
+ * "$Id: escp2-resolutions.c,v 1.43 2007/09/02 18:14:00 rlk Exp $"
  *
  *   Print plug-in EPSON ESC/P2 driver for the GIMP.
  *
@@ -641,8 +641,8 @@ static const quality_t standard_qualities[] =
   { "High",        N_("High"),         0, 0, 0, 0, 720, 720 },
   { "Photo",       N_("Photo"),        1440, 720, 2880, 720, 1440, 720 },
   { "HighPhoto",   N_("Super Photo"),  1440, 1440, 2880, 1440, 1440, 1440 },
-  { "UltraPhoto",  N_("Ultra Photo"),  2880, 2880, 2880, 2880, 2880, 2880 },
-  { "Best",        N_("Best"),         720, 360, 0, 0, -1, -1 },
+  { "UltraPhoto",  N_("Ultra Photo"),  2880, 1440, 2880, 1440, 2880, 1440 },
+  { "Best",        N_("Best"),         720, 360, 0, 1440, -1, -1 },
 };
 
 DECLARE_QUALITY_LIST(standard);
@@ -656,8 +656,8 @@ static const quality_t p1_5_qualities[] =
   { "High",        N_("High"),         0, 0, 0, 0, 720, 720 },
   { "Photo",       N_("Photo"),        1440, 720, 1440, 720, 1440, 720 },
   { "HighPhoto",   N_("Super Photo"),  1440, 1440, 2880, 1440, 1440, 1440 },
-  { "UltraPhoto",  N_("Ultra Photo"),  2880, 2880, 2880, 2880, 2880, 2880 },
-  { "Best",        N_("Best"),         720, 360, 0, 0, -1, -1 },
+  { "UltraPhoto",  N_("Ultra Photo"),  2880, 1440, 2880, 1440, 2880, 1440 },
+  { "Best",        N_("Best"),         720, 360, 0, 1440, -1, -1 },
 };
 
 DECLARE_QUALITY_LIST(p1_5);
