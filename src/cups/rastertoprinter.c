f1de446a5f0c0b27b3e8c6ab63d171732bd371d2
rlk <rlk>
2007-12-27 23:44:34 +0000

    Emit simple informational messages that will help us debug problems in the CUPS driver.

--- src/cups/rastertoprinter.c
+++ src/cups/rastertoprinter.c
@@ -1,5 +1,5 @@
 /*
- * "$Id: rastertoprinter.c,v 1.116 2007/12/24 02:54:12 rlk Exp $"
+ * "$Id: rastertoprinter.c,v 1.117 2007/12/27 23:44:34 rlk Exp $"
  *
  *   Gutenprint based raster filter for the Common UNIX Printing System.
  *
@@ -814,8 +814,6 @@ main(int  argc,				/* I - Number of command-line arguments */
  /*
   * Check for valid arguments...
   */
-  if (! suppress_messages)
-    fprintf(stderr, "DEBUG: Gutenprint %s Starting\n", version_id);
 
   if (argc < 6 || argc > 7)
   {
@@ -838,7 +836,15 @@ main(int  argc,				/* I - Number of command-line arguments */
     return (1);
   }
   if (! suppress_messages)
-    fprintf(stderr, "DEBUG: Gutenprint using PPD file %s\n", ppdfile);
+    {
+      fprintf(stderr, "INFO: Gutenprint command line: %s '%s' '%s' '%s' '%s' '%s'%s%s%s\n",
+	      argv[0], argv[1], argv[2], argv[3], argv[4], argv[5],
+	      argc >= 7 ? " '" : "",
+	      argc >= 7 ? argv[6] : "",
+	      argc >= 7 ? "'" : "");
+      fprintf(stderr, "INFO: Gutenprint using PPD file %s\n", ppdfile);
+      fprintf(stderr, "INFO: Gutenprint %s Starting\n", version_id);
+    }
 
   if ((ppd = ppdOpenFile(ppdfile)) == NULL)
   {
@@ -1359,5 +1365,5 @@ Image_width(stp_image_t *image)	/* I - Image */
 
 
 /*
- * End of "$Id: rastertoprinter.c,v 1.116 2007/12/24 02:54:12 rlk Exp $".
+ * End of "$Id: rastertoprinter.c,v 1.117 2007/12/27 23:44:34 rlk Exp $".
  */
